842225277096d3e833bcf8b27c85bae48501261d
danielpartida <daniel.partida@d-fine.de>
2022-04-20 12:31:21 +0200

    Run reduced version of Bert sentiment analysis

--- src/bert.py
+++ src/bert.py
@@ -1,16 +1,26 @@
+# Motivation from https://github.com/nicknochnack/BERTSentiment/blob/main/Sentiment.ipynb and
+# https://huggingface.co/finiteautomata/bertweet-base-sentiment-analysis?text=I+like+you.+I+love+you
 from transformers import pipeline
+import pandas as pd
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
 
-# Set up the inference pipeline using a model from the 🤗 Hub
-sentiment_analysis = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
 
-# Let's run the sentiment analysis on each tweet
-tweets = []
-for tweet in search:
-    try:
-        content = tweet.full_text
-        sentiment = sentiment_analysis(content)
-        tweets.append({'tweet': content, 'sentiment': sentiment[0]['label']})
-
+def sentiment_score(review):
+    tokens = tokenizer.encode(review, return_tensors='pt')
+    result = model(tokens)
+    return int(torch.argmax(result.logits))+1
 
 if __name__ == "__main__":
-    pass
+    tokenizer = AutoTokenizer.from_pretrained('finiteautomata/bertweet-base-sentiment-analysis')
+    model = AutoModelForSequenceClassification.from_pretrained('finiteautomata/bertweet-base-sentiment-analysis')
+
+    # TODO: Check other variant to load model
+    # sentiment_analysis = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+
+    df_tweets = pd.read_csv('../data/crypto.csv', encoding='latin')
+    df_reduced_tweets = df_tweets[:50]
+
+    df_reduced_tweets["result"] = df_reduced_tweets["tweet"].apply(lambda x: sentiment_score(x))
+
+    df_reduced_tweets.to_csv("../data/bert.csv", sep=";", decimal=",")
