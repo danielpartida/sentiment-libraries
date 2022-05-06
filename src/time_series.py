79e742b681a7f07065459033c4a79db350d90238
danielpartida <daniel.partida@d-fine.de>
2022-05-06 14:44:14 +0200

    Sort dataframe by creation date

--- src/time_series.py
+++ src/time_series.py
@@ -3,9 +3,12 @@ from datetime import datetime
 
 if __name__ == "__main__":
 
+    # FIXME: Change hard-coded import string
     df = pd.read_csv("../data/staratlas_sentiment_06-05-2022-13-46.csv", sep=';', decimal=',')
 
-    print(df.head())
+    df.sort_values(by=['created_at'], ascending=True, inplace=True)
     df['date'] = df['created_at'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S%z'))
     df['date'] = df['date'].apply(lambda x: x.strftime('%d/%m/%Y'))
-    print('Done')
+
+    # FIXME: Change hard-coded export string
+    df.to_csv("../data/staratlas_sentiment_06-05-2022-13-46_sorted.csv", sep=';', decimal=',')
