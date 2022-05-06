451c12fea6f32898e0120dbcae15864eef964adf
danielpartida <daniel.partida@d-fine.de>
2022-05-06 14:53:51 +0200

    Add groupby date and model_type sentiment

--- src/time_series.py
+++ src/time_series.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
     df['date'] = df['created_at'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S%z'))
     df['date'] = df['date'].apply(lambda x: x.strftime('%d/%m/%Y'))
 
-    # FIXME: Change hard-coded export string
-    df.to_csv("../data/staratlas_sentiment_06-05-2022-13-46_sorted.csv", sep=';', decimal=',')
+    # group-by
+    bert = df.groupby(by=['date', 'sentiment_bert'])['sentiment_bert'].count()
+    roberta = df.groupby(by=['date', 'sentiment_roberta'])['sentiment_roberta'].count()
