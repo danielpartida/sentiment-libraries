bf80951895e99793bee7bd95aa8ce7367dcfd88c
danielpartida <daniel.partida@d-fine.de>
2022-05-09 15:56:26 +0200

    Create abstract class with two extra classes
    TwitterScraper
    TwitterSentiment

--- src/run.py
+++ src/run.py
@@ -1,8 +1,11 @@
-from twitter_scraper import TwitterScraper
+from twitter import TwitterScraper, TwitterSentiment
 
 if __name__ == "__main__":
 
     search_term = "olympusdao"
-    tweet_scraper = TwitterScraper(search_term=search_term, limit_tweets=100, tweet_type="mixed")
-    df_tweets = tweet_scraper.get_scraped_tweets()
+    twitter_scraper = TwitterScraper(search_term=search_term, limit_tweets=100, tweet_type="mixed")
+    df_tweets = twitter_scraper.get_scraped_tweets()
     df_tweets.head()
+
+    twitter_sentiment_analysis = TwitterSentiment(search_term=search_term, model="roberta", df_tweet=df_tweets)
+    twitter_sentiment_analysis.run_sentiment_analysis()
