e3dc342d3e00b6baecaf9d4165347914232b2756
danielpartida <daniel.partida@d-fine.de>
2022-05-23 18:12:15 +0200

    Implement and run Reddit scraper for stepn

--- src/hubert_reddt_example/reddit.py
+++ src/hubert_reddt_example/reddit.py
@@ -1,12 +1,31 @@
 import datetime as dt
 
+import pandas as pd
 from psaw import PushshiftAPI
+from tqdm import tqdm
 
 api = PushshiftAPI()
 
-start_epoch=int(dt.datetime(2022, 5, 1).timestamp())
+start_epoch = int(dt.datetime(2022, 5, 1).timestamp())
 
-gen = api.search_comments(q="stepn", after=start_epoch, subreddit='StepN', limit=10)
+search_term = "stepn"
 
-for comment in gen:
-    print(comment.body)
+# FIXME: Change hardcoded subreddit
+gen = api.search_comments(q=search_term, after=start_epoch, subreddit='StepN', limit=10000)
+
+list_dict_comments = []
+for comment in tqdm(gen):
+    dict_comment = {
+        "author": comment.author,
+        "body": comment.body,
+        "controversiality": comment.controversiality,
+        "created": dt.datetime.fromtimestamp(comment.created),
+        "link_id": comment.link_id,
+        "permalink": comment.permalink
+    }
+
+    list_dict_comments.append(dict_comment)
+
+df_reddit = pd.DataFrame(list_dict_comments)
+df_reddit.to_csv("../../data/reddit/{0}_reddit.csv".format(search_term),
+                 sep=";", decimal=',')
