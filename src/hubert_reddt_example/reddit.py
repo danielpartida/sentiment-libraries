92ea0617e433490d22230bfc963aff34174f56d0
danielpartida <daniel.partida@d-fine.de>
2022-05-30 09:07:36 +0200

    Implement sentiment analysis for Reddit

--- src/hubert_reddt_example/reddit.py
+++ src/hubert_reddt_example/reddit.py
@@ -1,31 +1,83 @@
-import datetime as dt
+from datetime import datetime, date
 
 import pandas as pd
 from psaw import PushshiftAPI
 from tqdm import tqdm
+from transformers import pipeline
 
-api = PushshiftAPI()
 
-start_epoch = int(dt.datetime(2022, 5, 1).timestamp())
+def get_generator(start_epoch=int(datetime(2022, 5, 1).timestamp()),
+                  search_term: str = "stepn", subreddit: str = "stepn"):
+    api = PushshiftAPI()
 
-search_term = "solana"
+    start_epoch = start_epoch
+    search_term = search_term
+    generator = api.search_comments(q=search_term, after=start_epoch, subreddit=subreddit, limit=10000)
 
-# FIXME: Change hardcoded subreddit
-gen = api.search_comments(q=search_term, after=start_epoch, subreddit='solana', limit=10000)
+    return generator
 
-list_dict_comments = []
-for comment in tqdm(gen):
-    dict_comment = {
-        "author": comment.author,
-        "body": comment.body,
-        "controversiality": comment.controversiality,
-        "created": dt.datetime.fromtimestamp(comment.created),
-        "link_id": comment.link_id,
-        "permalink": comment.permalink
-    }
 
-    list_dict_comments.append(dict_comment)
+def get_dataframe_reddit_comments(generator) -> pd.DataFrame:
+    list_dict_comments = []
+    for comment in tqdm(generator):
+        dict_comment = {
+            "author": comment.author,
+            "text": comment.body,
+            # FIXME: handle missing controversiality
+            # "controversiality": comment.controversiality,
+            "created": datetime.fromtimestamp(comment.created),
+            "link_id": comment.link_id,
+            "permalink": comment.permalink,
+            "score": comment.score
+        }
 
-df_reddit = pd.DataFrame(list_dict_comments)
-df_reddit.to_csv("../../data/reddit/{0}_reddit.csv".format(search_term),
-                 sep=";", decimal=',')
+        list_dict_comments.append(dict_comment)
+
+    df_reddit_comments = pd.DataFrame(list_dict_comments)
+
+    return df_reddit_comments
+
+
+def compute_sentiment_analysis(df: pd.DataFrame, model: str) -> pd.DataFrame:
+    sentiment_analysis = pipeline(model=model_hugging_face)
+    df['sentiment_dict'] = df["text"].apply(lambda x: sentiment_analysis(x[:512]))
+    df['sentiment_{0}'.format(model)] = df["sentiment_dict"].apply(
+        lambda x: x[0]['label'])
+
+    df.to_csv("../../data/reddit/{0}_reddit_{1}.csv".format(reddit_search_term, today_string),
+              sep=";", decimal=',')
+
+    return df
+
+
+def calculate_timeseries_analysis(df: pd.DataFrame, model: str) -> None:
+    df["date"] = df.created.apply(lambda x: date(x.year, x.month, x.day))
+
+    # group-by
+    df.sort_values(by=["date"], ascending=True, inplace=True)
+
+    # group-by and pivot
+    model_group_by = df.groupby(by=['date', 'sentiment_{0}'.format(model)])[
+        'sentiment_{0}'.format(model)
+    ].count()
+    model_unstack = model_group_by.unstack()
+
+    model_unstack.to_csv("../../data/reddit/{0}_reddit_timeseries_{1}_{2}.csv".format(
+        reddit_search_term, model, today_string), sep=";", decimal=',')
+
+
+if __name__ == "__main__":
+    today = datetime.today()
+    today_string = today.strftime('%d-%m-%Y-%H-%M')
+
+    reddit_search_term = "stepn"
+    reddit_generator = get_generator(start_epoch=int(datetime(2022, 1, 1).timestamp()),
+                                     search_term=reddit_search_term, subreddit="stepn")
+
+    df_reddit = get_dataframe_reddit_comments(generator=reddit_generator)
+
+    model_hugging_face = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+    model_str = "roberta"
+
+    df_sentiment = compute_sentiment_analysis(df=df_reddit, model=model_str)
+    calculate_timeseries_analysis(df=df_sentiment, model=model_str)
