56eee7d4d3f0749552cfe9daa5297fe92e3ba3eb
danielpartida <daniel.partida@d-fine.de>
2022-05-30 10:08:05 +0200

    Add and compute mean score for sentiment analysis

--- src/reddit.py
+++ src/reddit.py
@@ -28,7 +28,7 @@ def get_dataframe_reddit_comments(generator) -> pd.DataFrame:
             "created": datetime.fromtimestamp(comment.created),
             "link_id": comment.link_id,
             "permalink": comment.permalink,
-            "score": comment.score
+            # "score": comment.score
         }
 
         list_dict_comments.append(dict_comment)
@@ -43,6 +43,8 @@ def compute_sentiment_analysis(df: pd.DataFrame, model: str) -> pd.DataFrame:
     df['sentiment_dict'] = df["text"].apply(lambda x: sentiment_analysis(x[:512]))
     df['sentiment_{0}'.format(model)] = df["sentiment_dict"].apply(
         lambda x: x[0]['label'])
+    df['score_{0}'.format(model)] = df["sentiment_dict"].apply(
+        lambda x: x[0]['score'])
 
     df.to_csv("../data/reddit/{0}_reddit_{1}.csv".format(reddit_search_term, today_string),
               sep=";", decimal=',')
@@ -53,19 +55,23 @@ def compute_sentiment_analysis(df: pd.DataFrame, model: str) -> pd.DataFrame:
 def calculate_timeseries_analysis(df: pd.DataFrame, model: str) -> None:
     df["date"] = df.created.apply(lambda x: date(x.year, x.month, x.day))
 
-    # group-by
+    # sort values
     df.sort_values(by=["date"], ascending=True, inplace=True)
 
     # group-by and pivot
     model_group_by = df.groupby(by=['date', 'sentiment_{0}'.format(model)])[
-        'sentiment_{0}'.format(model)
-    ].count()
+        'sentiment_{0}'.format(model)].count()
     model_unstack = model_group_by.unstack()
-
     df_pivot = pd.DataFrame(model_unstack)
     df_pivot["sum"] = model_unstack.sum(axis=1)
+
+    # Calculate relative sentiments
     df_pivot_rel = df_pivot[['Positive', 'Negative', 'Neutral']].div(df_pivot["sum"], axis=0)
     df_pivot_rel["total_posts"] = df_pivot["sum"]
+
+    # Calculate mean probability of sentiment
+    df_probability = df.groupby('date', as_index=False)["score_{0}".format(model)].mean()
+    df_pivot_rel["score"] = df_probability["score_{0}".format(model)].values
 
     df_pivot_rel.to_csv("../data/reddit/{0}_reddit_timeseries_{1}_{2}.csv".format(
         reddit_search_term, model, today_string), sep=";", decimal=',')
@@ -76,7 +82,7 @@ if __name__ == "__main__":
     today_string = today.strftime('%d-%m-%Y-%H-%M')
 
     reddit_search_term = "stepn"
-    reddit_generator = get_generator(start_epoch=int(datetime(2022, 5, 27).timestamp()),
+    reddit_generator = get_generator(start_epoch=int(datetime(2022, 1, 1).timestamp()),
                                      search_term=reddit_search_term, subreddit="stepn")
 
     df_reddit = get_dataframe_reddit_comments(generator=reddit_generator)
