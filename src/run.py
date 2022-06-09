350f98a4c0abd369f86fdfe66cba17905f7165f9
danielpartida <daniel.partida@d-fine.de>
2022-06-09 17:09:14 +0200

    Add context annotation for crypto

--- src/run.py
+++ src/run.py
@@ -3,7 +3,7 @@ from twitter import TwitterScraper, TwitterSentiment
 
 if __name__ == "__main__":
 
-    search_term = "context:11.689566306014617600"
+    search_term = "context:66.913142676819648512"
     delta_days = 7
     from_time = datetime.utcnow() - timedelta(days=delta_days)
     until_time = datetime.utcnow() - timedelta(hours=1)
