7856cc31223edd897d6df3b0aacabc80f9423eac
danielpartida <daniel.partida@d-fine.de>
2022-06-09 16:46:47 +0200

    Scrap tweets for the ravens

--- src/run.py
+++ src/run.py
@@ -3,7 +3,7 @@ from twitter import TwitterScraper, TwitterSentiment
 
 if __name__ == "__main__":
 
-    search_term = "solana"
+    search_term = "context:11.689566306014617600"
     delta_days = 7
     from_time = datetime.utcnow() - timedelta(days=delta_days)
     until_time = datetime.utcnow() - timedelta(hours=1)
