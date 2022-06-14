3b7712d2c8909e3444669c2f55e2543267a7345f
danielpartida <daniel.partida@d-fine.de>
2022-06-14 16:44:49 +0200

    Create algorithm to fetch look-up table

--- src/look_up_annotations.py
+++ src/look_up_annotations.py
@@ -6,5 +6,30 @@ if __name__ == "__main__":
 
     data = pd.read_csv("../data/backup/crypto_context_tweets_09_06_17_05.csv", sep=';', decimal=',')
     context_annotations = data.context_annotations
-    series_annotations = context_annotations.apply(lambda x: eval(x)) # convert str to list
-    print(series_annotations)
+    # convert str to list
+    series_annotations = context_annotations.apply(lambda x: eval(x))
+
+    try:
+        look_up_dict = {"domains": {}, "entities": {}}
+        for row in series_annotations:
+            for element in row:
+                id_domain = int(element["domain"]["id"])
+                if id_domain in look_up_dict["domains"].keys():
+                    continue
+
+                look_up_dict["domains"][id_domain] = {}
+                look_up_dict["domains"][id_domain]["name"] = element["domain"]["name"]
+
+                if "description" in element["domain"].keys():
+                    look_up_dict["domains"][id_domain]["description"] = element["domain"]["description"]
+
+                id_entity = int(element["entity"]["id"])
+                if id_entity in look_up_dict["entities"].keys():
+                    continue
+
+                look_up_dict["entities"][id_entity] = {}
+                look_up_dict["entities"][id_entity]["name"] = element["entity"]["name"]
+    except Exception as e:
+        print(e)
+
+    print(look_up_dict)
