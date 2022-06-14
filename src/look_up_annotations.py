3630193ad41de211c5dfd95ecf066aa85150f569
danielpartida <daniel.partida@d-fine.de>
2022-06-14 17:00:37 +0200

    Simplify look-up table

--- src/look_up_annotations.py
+++ src/look_up_annotations.py
@@ -10,26 +10,26 @@ if __name__ == "__main__":
     series_annotations = context_annotations.apply(lambda x: eval(x))
 
     try:
-        look_up_dict = {"domains": {}}
+        look_up_dict = {}
         for row in series_annotations:
             for element in row:
 
                 id_domain = int(element["domain"]["id"])
-                if id_domain not in look_up_dict["domains"].keys():
-                    look_up_dict["domains"][id_domain] = {}
-                    look_up_dict["domains"][id_domain]["name"] = element["domain"]["name"]
+                if id_domain not in look_up_dict.keys():
+                    look_up_dict[id_domain] = {}
+                    look_up_dict[id_domain]["name"] = element["domain"]["name"]
 
                     if "description" in element["domain"].keys():
-                        look_up_dict["domains"][id_domain]["description"] = element["domain"]["description"]
+                        look_up_dict[id_domain]["description"] = element["domain"]["description"]
 
-                    look_up_dict["domains"][id_domain]["entities"] = {}
+                    look_up_dict[id_domain]["entities"] = {}
 
                 id_entity = int(element["entity"]["id"])
-                if id_entity in look_up_dict["domains"][id_domain]["entities"].keys():
+                if id_entity in look_up_dict[id_domain]["entities"].keys():
                     continue
 
-                look_up_dict["domains"][id_domain]["entities"][id_entity] = {}
-                look_up_dict["domains"][id_domain]["entities"][id_entity]["name"] = element["entity"]["name"]
+                look_up_dict[id_domain]["entities"][id_entity] = {}
+                look_up_dict[id_domain]["entities"][id_entity]["name"] = element["entity"]["name"]
 
     except Exception as e:
         print(e)
