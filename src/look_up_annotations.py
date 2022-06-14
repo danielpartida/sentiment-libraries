904133bb6e7291388b0c56fceeaf7564318487d4
danielpartida <daniel.partida@d-fine.de>
2022-06-14 15:27:55 +0200

    Convert annotations to a list of annotations

--- src/look_up_annotations.py
+++ src/look_up_annotations.py
@@ -4,11 +4,7 @@ import json
 
 if __name__ == "__main__":
 
-    data = pd.read_csv("data/backup/crypto_context_tweets_09_06_17_05.csv", sep=';', decimal=',')
+    data = pd.read_csv("../data/backup/crypto_context_tweets_09_06_17_05.csv", sep=';', decimal=',')
     context_annotations = data.context_annotations
-    
-    result = context_annotations.to_json(orient="values")
-    # parsed = json.loads(result)
-
-    out_file = open("data/backup/result.json", "w")
-    json.dump(result, out_file, indent=4) 
+    series_annotations = context_annotations.apply(lambda x: eval(x)) # convert str to list
+    print(series_annotations)
