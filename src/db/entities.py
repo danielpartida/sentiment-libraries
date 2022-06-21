b4a89efddb22a544496348e8a3b3bbc4549bc98c
danielpartida <daniel.partida@d-fine.de>
2022-06-21 10:16:52 +0200

    Build TwitterStreams class for db table
    Table is declared using sqlalchemy ORM

---
+++ src/db/entities.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+
+from utils import get_engine
+
+load_dotenv()
+
+Base = declarative_base()
+
+
+class TwitterStreams(Base):
+    __tablename__ = 'twitter_streams'
+    domain_id = Column(Integer)
+    entity_id = Column(Integer)
+    entity_name = Column(String)
+    language = Column(String)
+    text = Column(String)
+    tweet_id = Column(Integer, primary_key=True, autoincrement=False)
+
+
+if __name__ == "__main__":
+    engine = get_engine()
+    Base.metadata.create_all(engine)
