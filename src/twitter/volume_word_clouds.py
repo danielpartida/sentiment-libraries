d12ec94ef8c4df3be9a0bfaf4f02dd239fd2b3b1
danielpartida <daniel.partida@d-fine.de>
2022-06-22 12:20:10 +0200

    Refactor query to a method

--- src/twitter/volume_word_clouds.py
+++ src/twitter/volume_word_clouds.py
@@ -3,12 +3,32 @@ from datetime import datetime, timedelta
 from src.db.helper import get_session
 from src.db.entities import TwitterStreams
 
+
+def compute_word_clouds():
+    pass
+
+
+def query_last_streams(time_delta: timedelta) -> TwitterStreams:
+    """
+    Fetches last tweets using sqlalchemy ORM
+    :param time_delta: time_delta e.g. datetime.now() - timedelta(hours=12)
+    :type time_delta: timedelta
+    :return: last twitter streams
+    :rtype: TwitterStreams
+    """
+    last_streams = session.query(TwitterStreams).filter(
+        TwitterStreams.domain_id == 66, TwitterStreams.date >= time_delta
+    ).order_by(TwitterStreams.id.asc()).all()
+
+    return last_streams
+
+
 if __name__ == "__main__":
 
     session = get_session()
-    test = session.query(TwitterStreams).filter(
-        TwitterStreams.domain_id == 66, TwitterStreams.date >= datetime.now() - timedelta(hours=12)
-    ).order_by(TwitterStreams.id.asc()).all()
+
+    delta = datetime.now() - timedelta(hours=12)
+    last_tweets = query_last_streams(time_delta=delta)
 
     session.close()
 
