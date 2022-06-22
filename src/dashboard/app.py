88f8f6da6c54372134bc6839a7342d24b8588c89
danielpartida <daniel.partida@d-fine.de>
2022-06-22 17:08:35 +0200

    Make multipages

---
+++ src/dashboard/app.py
@@ -0,0 +1,14 @@
+# Good starting point https://github.com/ucg8j/awesome-dash
+# Getting started https://docs.streamlit.io/library/get-started/
+# Layout: https://blog.streamlit.io/introducing-new-layout-options-for-streamlit/
+# Wordcloud: https://discuss.streamlit.io/t/how-to-add-wordcloud-graph-in-streamlit/818/3
+
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+# Run command "py -m streamlit run app.py"
+if __name__ == "__main__":
+
+    st.markdown("# Mooonpass Dashboard 🌚🧭")
+    st.sidebar.markdown("# Main page 🌚🧭")
---
+++ src/dashboard/pages/top_projects.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("# Top Projects 🚀")
+st.sidebar.markdown("# Top Projects 🚀")
