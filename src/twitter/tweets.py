a1c915792f2d0131102256e82258ec4d28ee3c30
danielpartida <daniel.partida@d-fine.de>
2022-06-27 17:49:55 +0200

    Create search tweets file

--- src/twitter/tweets.py
+++ src/twitter/tweets.py
@@ -1,6 +1,6 @@
 import os
-from typing import Tuple
 from datetime import datetime
+from typing import Tuple
 
 import pandas as pd
 import requests
@@ -28,18 +28,14 @@ def get_authentication_headers(academic_access: bool) -> dict:
     return headers
 
 
-def build_url(request_type: str = "counts", access_level: str = "all", granularity_level: str = "day",
-              start_date: str = "2022-01-01T00:00:00Z", end_date: str = "2022-06-15T00:00:00Z",
-              search_term: str = "bitcoin", next_token_id: str = "") -> str:
+def build_url(request_type: str = "search", access_level: str = "all", start_date: str = "2022-01-01T00:00:00Z",
+              end_date: str = "2022-06-15T00:00:00Z", search_term: str = "bitcoin", next_token_id: str = "") -> str:
     """
-    Builds url to request counts or tweets
-    Source and examples https://developer.twitter.com/en/docs/twitter-api/tweets/counts/quick-start/recent-tweet-counts
-    :param request_type: "counts" or "search"
+    Source: https://developer.twitter.com/en/docs/twitter-api/tweets/search/api-reference/get-tweets-search-all
+    :param request_type: "search"
     :type request_type: str
     :param access_level: academic "all" or premium "recent"
     :type access_level: str
-    :param granularity_level: day, hour or minute
-    :type granularity_level: str
     :param start_date: format 2022-01-01T00:00:00Z
     :type start_date: str
     :param end_date: format 2022-06-15T00:00:00Z
@@ -56,20 +52,20 @@ def build_url(request_type: str = "counts", access_level: str = "all", granulari
 
     # last run
     if not next_token_id:
-        url = "{0}/{1}/{2}?query={3}&granularity={4}&start_time={5}&end_time={6}".format(
-            basic_url, request_type, access_level, search_term, granularity_level, start_date, end_date
+        url = "{0}/{1}/{2}?query={3}&start_time={4}&end_time={5}&max_results=500".format(
+            basic_url, request_type, access_level, search_term, start_date, end_date
         )
 
     # first run
     elif next_token_id == "first_run":
-        url = "{0}/{1}/{2}?query={3}&granularity={4}&start_time={5}&end_time={6}".format(
-            basic_url, request_type, access_level, search_term, granularity_level, start_date, end_date
+        url = "{0}/{1}/{2}?query={3}&start_time={4}&end_time={5}&max_results=500".format(
+            basic_url, request_type, access_level, search_term, start_date, end_date
         )
 
     # all runs besides first and last run
     else:
-        url = "{0}/{1}/{2}?query={3}&granularity={4}&start_time={5}&end_time={6}&next_token={7}".format(
-            basic_url, request_type, access_level, search_term, granularity_level, start_date, end_date, next_token_id
+        url = "{0}/{1}/{2}?query={3}&start_time={4}&end_time={5}&next_token={6}&max_results=500".format(
+            basic_url, request_type, access_level, search_term, start_date, end_date, next_token_id
         )
 
     return url
@@ -103,7 +99,7 @@ def get_data_with_control_flow(request_response: requests.models.Response, reque
 
 def convert_data_into_df(data: dict) -> Tuple:
     """
-    Builds dataframe with response data, fetches the total tweets and gets next_token_id
+    Builds dataframe with response data and gets next_token_id
     :param data: response data
     :type data: dict
     :return: df of timeseries of total tweets per granularity, total tweets in that window, and next token id
@@ -115,36 +111,17 @@ def convert_data_into_df(data: dict) -> Tuple:
     else:
         next_token_id = None
 
-    total_tweet_count = data["meta"]["total_tweet_count"]
     df_tweets = pd.DataFrame(data=data["data"])
-    df_tweets["start"] = pd.to_datetime(df_tweets.start).dt.strftime('%Y-%m-%d %H:%M')
-    df_tweets["end"] = pd.to_datetime(df_tweets.end).dt.strftime('%Y-%m-%d %H:%M')
-    df_tweets["dates"] = [pd.to_datetime(d) for d in df_tweets.start]
 
-    return df_tweets, total_tweet_count, next_token_id
-
-
-# TODO: Set dynamically entity_id and annotation_id
-# def build_url_with_annotation():
-#     # entity_id = 174  # digital asset
-#     # annotation_id = 1007360414114435072  # Bitcoin
-#     # query_annotation = "context:{0}.{1}".format(entity_id, annotation_id)
-#     pass
+    return df_tweets, next_token_id
 
 
 if __name__ == "__main__":
 
-    df = pd.read_csv("../../look_up_tables/df_Digital Assets & Crypto.csv", sep=";", decimal=',')
-    df.rename(columns={"Unnamed: 0": "id"}, inplace=True)
-
-    # entities = pd.read_csv("../../look_up_tables/entities.csv", sep=';', decimal=',')
-    # entities.rename(columns={"Unnamed: 0": "id", "0": "name"}, inplace=True)
-
     # Build header to authenticate using bearer token
     authentication_header = get_authentication_headers(academic_access=True)
 
-    query_type = "counts"
-    granularity = "day"  # day, hour or min
+    query_type = "search"
 
     start = "2022-01-01T00:00:00Z"
     today = datetime.today()
@@ -153,27 +130,25 @@ if __name__ == "__main__":
     end = today.strftime(date_format_long)
 
     access_type = "all"  # "all" for academic access, "recent" for premium access
-    query_text = "solana"
+    query_text = "solana -is:retweet"
 
     all_df_tweets = []
     total_tweets = 0
     next_token = "first_run"
-    while next_token:
-        constructed_url = build_url(request_type=query_type, access_level=access_type, granularity_level=granularity,
-                                    start_date=start, end_date=end, search_term=query_text, next_token_id=next_token)
+
+    rate_limit = 00
+    while next_token and rate_limit < 5:
+        constructed_url = build_url(request_type=query_type, access_level=access_type, start_date=start, end_date=end,
+                                    search_term=query_text, next_token_id=next_token)
 
         response = requests.get(url=constructed_url, headers=authentication_header)
         response_data = get_data_with_control_flow(request_response=response, request_url=constructed_url)
 
-        df_tweets_window, total_tweets_window, next_token = convert_data_into_df(response_data)
+        df_tweets_window, next_token = convert_data_into_df(response_data)
         all_df_tweets.append(df_tweets_window)
-        total_tweets += total_tweets_window
+
+        rate_limit += 1
 
     df = pd.concat(all_df_tweets)
-    df.set_index("dates", inplace=True)
     df.sort_index(inplace=True)
     df = df.tweet_count
-
-    df.to_csv("../dashboard/data/{0}_{1}.csv".format(query_text, today.strftime(date_format_short)),
-              decimal=',', sep=';')
-    print("Run")
