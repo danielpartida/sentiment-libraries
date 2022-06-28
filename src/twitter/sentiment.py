057cb9d81aa667bd419880b3b0763475edd85829
danielpartida <daniel.partida@d-fine.de>
2022-06-28 15:59:20 +0200

    Implement sentiment analysis script

--- src/twitter/sentiment.py
+++ src/twitter/sentiment.py
@@ -1,15 +1,51 @@
 import pandas as pd
 
+from transformers import pipeline
+from datetime import datetime, date
 
-def calculate_sentiment_analysis(df: pd.DataFrame) -> None:
+
+def calculate_sentiment_analysis(df: pd.DataFrame, token: str) -> pd.DataFrame:
     """
-    Performs sentiment analysis depending on the model
+    Calculates sentiment analysis based on RoBERTa architecture cardiffnlp/twitter-roberta-base-sentiment-latest
+    other alternative is using BERT: finiteautomata/bertweet-base-sentiment-analysis
+    :param df: dataframe of scraped tweets
+    :type df: pd.DataFrame
+    :param token: asset name
+    :type token: str
+    :return: dataframe with sentiment analysis
+    :rtype: pd.DataFrame
     """
+    model = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+    sentiment_analysis = pipeline(model=model)
     df['sentiment_dict'] = df["text"].apply(lambda x: sentiment_analysis(x))
-    df['sentiment_{0}'.format(model_str)] = df["sentiment_dict"].apply(lambda x: x[0]['label'])
-    df['score_{0}'.format(model_str)] = df["sentiment_dict"].apply(lambda x: x[0]['score'])
+    df['sentiment'] = df["sentiment_dict"].apply(lambda x: x[0]['label'])
+    df['score'] = df["sentiment_dict"].apply(lambda x: x[0]['score'])
+    df.to_csv("../dashboard/data/sentiment_tweets_{0}_{1}.csv".format(token, today.strftime(date_format_short)),
+              sep=";", decimal=",")
+
+    return df
+
+
+def calculate_timeseries_analysis(df: pd.DataFrame, token: str) -> None:
+    df["date"] = df.created_at.apply(lambda x: date(x.year, x.month, x.day))
+
+    # group-by
+    df.sort_values(by=["date"], ascending=True, inplace=True)
+
+    # group-by and pivot
+    model_group_by = df.groupby(by=['date', 'sentiment'])['sentiment'].count()
+    model_unstack = model_group_by.unstack()
+
+    model_unstack.to_csv(
+        "../dashboard/data/sentiment_timeseries_{0}_sentiment_{1}.csv".format(token, today.strftime(date_format_short)),
+        sep=';', decimal=',')
 
 
 if __name__ == "__main__":
+    today = datetime.today()
+    date_format_short = '%d_%m'
+    asset = "solana"
 
-    print("Run")
+    data = pd.read_csv("../dashboard/data/tweets_{0}_{1}.csv".format(asset, today.strftime(date_format_short)),
+                       sep=';', decimal=',')
+    df_sentiment = calculate_sentiment_analysis(df=data, token=asset)
