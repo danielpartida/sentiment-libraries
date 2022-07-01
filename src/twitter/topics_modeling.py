a3aa3c3465bc7d27bffc46f70e5a18643ddfd728
danielpartida <daniel.partida@d-fine.de>
2022-07-01 16:00:22 +0200

    Add topic modelling

---
+++ src/twitter/topics_modeling.py
@@ -0,0 +1,92 @@
+from typing import List, Tuple, Any
+
+import pandas as pd
+import nltk
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+import re
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+from nltk import ngrams
+
+
+def remove_links(tweet):
+    """Takes a string and removes web links from it"""
+    tweet = re.sub(r'http\S+', '', tweet)  # remove http links
+    tweet = re.sub(r'bit.ly/\S+', '', tweet)  # rempve bitly links
+    tweet = tweet.strip('[link]')  # remove [links]
+    return tweet
+
+
+def remove_users(tweet):
+    """Takes a string and removes retweet and @user information"""
+    tweet = re.sub('(RT\s@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)  # remove retweet
+    tweet = re.sub('(@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)  # remove tweeted at
+    return tweet
+
+
+# cleaning master function
+def clean_tweet(tweet, bigrams=False):
+    tweet = remove_users(tweet)
+    tweet = remove_links(tweet)
+    tweet = tweet.lower()  # lower case
+    tweet = re.sub('[' + my_punctuation + ']+', ' ', tweet)  # strip punctuation
+    tweet = re.sub('\s+', ' ', tweet)  # remove double spacing
+    tweet = re.sub('([0-9]+)', '', tweet)  # remove numbers
+    tweet_token_list = [word for word in tweet.split(' ')
+                        if word not in my_stopwords]  # remove stopwords
+
+    tweet_token_list = [word_rooter(word) if '#' not in word else word
+                        for word in tweet_token_list]  # apply word rooter
+    if bigrams:
+        tweet_token_list = tweet_token_list + [tweet_token_list[i] + '_' + tweet_token_list[i + 1]
+                                               for i in range(len(tweet_token_list) - 1)]
+    tweet = ' '.join(tweet_token_list)
+    return tweet
+
+
+def display_topics(model, feature_names, no_top_words):
+    topic_dict = {}
+    for topic_idx, topic in enumerate(model.components_):
+        topic_dict["Topic %d words" % (topic_idx)] = ['{}'.format(feature_names[i])
+                                                      for i in topic.argsort()[:-no_top_words - 1:-1]]
+        topic_dict["Topic %d weights" % (topic_idx)] = ['{:.1f}'.format(topic[i])
+                                                        for i in topic.argsort()[:-no_top_words - 1:-1]]
+    return pd.DataFrame(topic_dict)
+
+
+def get_top_n_gram(corpus, ngram_range, n=None):
+    vec = CountVectorizer(ngram_range=ngram_range, stop_words="english").fit(corpus)
+    bag_of_words = vec.transform(corpus)
+    sum_words = bag_of_words.sum(axis=0)
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
+    words_freq: list[tuple[Any, Any]] = sorted(words_freq, key=lambda x: x[1], reverse=True)
+    return words_freq[:n]
+
+
+if __name__ == "__main__":
+    # Source https://ourcodingclub.github.io/tutorials/topic-modelling-python/
+
+    my_stopwords = nltk.corpus.stopwords.words('english')
+    word_rooter = nltk.stem.snowball.PorterStemmer(ignore_stopwords=False).stem
+    my_punctuation = '!"$%&\'()*+,-./:;<=>?[\\]^_`{|}~•@'
+
+    df = pd.read_csv("../dashboard/data/tweets_solana_29_06.csv", sep=";", decimal=",", encoding="utf-8")
+
+    grams = get_top_n_gram(df.text, (4,4), 20)
+
+    df['clean_tweet'] = df.text.apply(clean_tweet)
+
+    vectorizer = CountVectorizer(max_df=0.9, min_df=25, token_pattern='\w+|\$[\d\.]+|\S+')
+    tf = vectorizer.fit_transform(df['clean_tweet']).toarray()
+
+    # tf_feature_names tells us what word each column in the metric represents
+    tf_feature_names = vectorizer.get_feature_names()
+    number_of_topics = 10
+    model = LatentDirichletAllocation(n_components=number_of_topics, random_state=0)
+    model.fit(tf)
+
+    no_top_words = 10
+    df_model = display_topics(model, tf_feature_names, no_top_words)
+
+    print("Run")
